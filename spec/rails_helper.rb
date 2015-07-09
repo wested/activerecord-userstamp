0e25e8ad2a73e355396903b478db7946ebd202f7
Joel Low <joel@joelsplace.sg>
2015-07-09 16:59:19 +0800

    Fix the failing rspec call.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 
 require File.expand_path('dummy/config/environment.rb', __dir__)
-require 'rails/test_help'
 require 'rspec/rails'
 
 Dir[__dir__ + '/support/**/*'].each { |f| require f if File.file?(f) }
